defae4f3d033a937976a6a8ad7305a2acc98aab4
Daniel Diaz <danieldiaz-123@hotmail.com>
2024-04-20 02:26:34 -0300

    styles NavBar

--- src/components/navbar/navBar.jsx
+++ src/components/navbar/navBar.jsx
@@ -1,14 +1,31 @@
 import styles from './navBar.module.css'
 
 function NavBar() {
+
+    let prevScroll = window.pageYOffset;
+
+    window.onscroll = () => {
+        let currScroll = window.pageYOffset;
+        if (prevScroll > currScroll) {
+            document.querySelector(`.${styles.Container}`).style.top = "0"
+            console.log(prevScroll)
+        } else {
+            document.querySelector(`.${styles.Container}`).style.top = "-100px"
+        }
+        prevScroll = currScroll;
+    }
+
     return (
         <>
             <div className={styles.Container}>
+
+                <div className={styles.boldNav}>
                 <p>HOME</p>
                 <p>ABOUT</p>
                 <p>SKILLS</p>
                 <p>PROJECTS</p>
                 <p>CONTACT</p>
+                </div>
 
                 <div className={styles.lightNav}>
                     <p>LN</p>
