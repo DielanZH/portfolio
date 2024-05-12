2ce23020728e1ba2b3a376d88951d69e0594862d
Daniel Diaz <danieldiaz-123@hotmail.com>
2024-05-11 23:51:28 -0300

    navBar CSS and resume

--- src/components/resume/resume.jsx
+++ src/components/resume/resume.jsx
@@ -16,11 +16,17 @@ export default function Resume({ translated }) {
                     'I am a Full Stack Developer specializing in Front-end development.'}
             </h2>
 
-            <p className={styles.presentation3}>
-                {translated ? 'Una de mis mayores motivaciones en este mundo es aprender. Así empecé mis años de autodidacta con tutoriales y repositorios de GitHub, hasta que tuve la fortuna de conocer el bootcamp de soyHenry. En esta etapa, además de desarrollar fuertemente mis habilidades como desarrollador, aprendí a trabajar en equipo de forma dinámica con metodologías ágiles, el manejo del flujo de GIT, estructura de datos, algoritmos y frameworks CSS.' :
-                    "One of my greatest motivations in this world is learning. I began my journey as a self-taught developer through tutorials and GitHub repositories until I was fortunate enough to discover the soyHenry bootcamp. During this stage, besides significantly developing my coding skills, I learned to work dynamically in a team using agile methodologies, managing the GIT workflow, understanding data structures, algorithms, and CSS frameworks."
+            <div className={styles.presentation3}>
+                {translated ?
+                    <p>
+                        Una de mis mayores motivaciones en este mundo es aprender. Así empecé mis años de autodidacta con tutoriales y repositorios de GitHub, hasta que tuve la fortuna de conocer el bootcamp de soyHenry.<br></br> En esta etapa, además de desarrollar fuertemente mis habilidades como desarrollador, aprendí a trabajar en equipo de forma dinámica con metodologías ágiles, el manejo del flujo de GIT, estructura de datos, algoritmos y frameworks CSS.
+                    </p>
+                    :
+                    <p>
+                        One of my greatest motivations in this world is learning. I began my journey as a self-taught developer through tutorials and GitHub repositories until I was fortunate enough to discover the soyHenry bootcamp.<br></br> During this stage, besides significantly developing my coding skills, I learned to work dynamically in a team using agile methodologies, managing the GIT workflow, understanding data structures, algorithms, and CSS frameworks.
+                    </p>
                 }
-            </p>
+            </div>
 
             <Link className={styles.CVButton} to="https://drive.google.com/file/d/1ZkWp7v0vFU0n5AX_ujMGY6xY5DhW6MhV/view?usp=sharing" target='_blank'>
                 <span className={styles.linkText}>
