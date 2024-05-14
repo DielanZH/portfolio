d74bf4d058ea2dc8857b640161cdaeb72f438c51
Daniel Diaz <danieldiaz-123@hotmail.com>
2024-05-14 00:58:51 -0300

    feat/sideBar finished AND CSS

--- src/components/sidebar/sideBar.jsx
+++ src/components/sidebar/sideBar.jsx
@@ -1,13 +1,15 @@
+import { Link } from 'react-router-dom'
 import styles from './sideBar.module.css'
 
 function SideBar() {
     return (
 
         <div className={styles.Container}>
-            <button className={styles.GitHub}></button>
-            <button className={styles.LinkedIn}></button>
-            <button className={styles.Mail}></button>
-            <button className={styles.PDFicon}></button>
+            <Link className={styles.GitHub} to={"https://github.com/DielanZH"} target='blank'></Link>
+            <Link className={styles.LinkedIn} to={"https://www.linkedin.com/in/daniel-diaz23"} target='blank'></Link>
+            <Link className={styles.Mail} to={"mailto:danieldiaz-123@hotmail.com"} target='blank'></Link>
+            <Link className={styles.PDFicon} to={"https://drive.google.com/file/d/1ZkWp7v0vFU0n5AX_ujMGY6xY5DhW6MhV/view"} target='blank'></Link>
+            <hr></hr>
         </div>
     )
 }
