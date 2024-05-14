1f043398f08049609fd60eaa7091717942a9ca15
Daniel Diaz <danieldiaz-123@hotmail.com>
2024-05-14 04:04:36 -0300

    SVG first try

--- src/components/sidebar/sideBar.jsx
+++ src/components/sidebar/sideBar.jsx
@@ -1,14 +1,16 @@
 import { Link } from 'react-router-dom'
 import styles from './sideBar.module.css'
+import gitHub from "src/assets/github-mark/github-mark/github-mark-white.svg"
+document.getElementById('github').style.backgroundImage = `url("${gitHub}")`
 
 function SideBar() {
     return (
 
         <div className={styles.Container}>
-            <Link className={styles.GitHub} to={"https://github.com/DielanZH"} target='blank'></Link>
-            <Link className={styles.LinkedIn} to={"https://www.linkedin.com/in/daniel-diaz23"} target='blank'></Link>
-            <Link className={styles.Mail} to={"mailto:danieldiaz-123@hotmail.com"} target='blank'></Link>
-            <Link className={styles.PDFicon} to={"https://drive.google.com/file/d/1ZkWp7v0vFU0n5AX_ujMGY6xY5DhW6MhV/view"} target='blank'></Link>
+            <Link className={styles.GitHub} id='github' to={"https://github.com/DielanZH"} target='blank'></Link>
+            <Link className={styles.LinkedIn} id='linkedin' to={"https://www.linkedin.com/in/daniel-diaz23"} target='blank'></Link>
+            <Link className={styles.Mail} id='mail' to={"mailto:danieldiaz-123@hotmail.com"} target='blank'></Link>
+            <Link className={styles.PDFicon} id='pdficon' to={"https://drive.google.com/file/d/1ZkWp7v0vFU0n5AX_ujMGY6xY5DhW6MhV/view"} target='blank'></Link>
             <hr></hr>
         </div>
     )
