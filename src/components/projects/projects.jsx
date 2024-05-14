1b4da915f621459e5f128acea8c4fdf15ff8e37b
Daniel Diaz <danieldiaz-123@hotmail.com>
2024-05-13 21:02:07 -0300

    Added InstaChef to projects

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -11,20 +11,24 @@ function Projects() {
                 {"//Mira los proyectos en los que trabajé recientemente"}
             </p>
 
-                <h2 className={styles.projectTittle}> App de recetas</h2>
+            <h2 className={styles.projectTittle}> App de recetas</h2>
             <div className={styles.Cards}>
 
-                <p className={styles.description}>Una SPA que consume datos de una API externa, y permite la visualización de información general y detallada de más de 500,000 videojuegos, entre las funcionalidades que dispone esta APP está el filtrar y ordenar videojuegos por diferentes variables como género, plataformas, nombre, calificación, además de la creación de nuevos videojuegos, mediante un formulario controlado.</p>
+                <p className={styles.description}>Una SPA que consume datos de una API externa, y permite la visualización de información general y detallada de más de 500,000 recetas, entre las funcionalidades que dispone esta APP está el filtrado y ordenamiento de recetas por diferentes variables como si son traidos de la API o la DB, healthscore, nombre, calificación, además de la creación de nuevas recetas mediante un formulario controlado.</p>
 
                 <img src="src\components\projects\images\recipes.png" alt="" />
 
                 <p className={styles.tecnologies}>
-                    React
-                    Redux
-                    Styled Components
+                    JavaScript
+                    React.js
+                    Redux.js
+                    <br></br>
+                    Node.js
                     Express
                     PostgreSQL
-                    Rawg.io API
+                    <br></br>
+                    Styled Components
+                    Spoonacular-API
                 </p>
                 <button className={styles.GitHub}></button>
                 <button className={styles.Deploy}></button>
@@ -40,17 +44,42 @@ function Projects() {
                 <img src="src\components\projects\images\component-corner.png" alt="" />
 
                 <p className={styles.tecnologies}>
+                    JavaScript
+                    Node.js
                     React
                     Redux
-                    Styled Components
                     Express
                     PostgreSQL
-                    Rawg.io API
+                    Styled Components
                 </p>
                 <button className={styles.GitHub}></button>
                 <button className={styles.Deploy}></button>
 
             </div>
+
+
+            <h2 className={styles.projectTittle}>Insta-Chef</h2>
+            <div className={styles.Cards}>
+
+                <p className={styles.description}>Colaboré como desarrollador fullstack en el proyecto grupal Insta-Chef, un e-commerce gastronómico artificial durante el bootcamp de Henry. Contribuí en la arquitectura y diseño de los componentes, enfocándome principalmente del lado front-end del proyecto.</p>
+
+                <img src="src\components\projects\images\InstaChef.png" alt="" />
+
+                <p className={styles.tecnologies}>
+                    JavaScript
+                    React
+                    Redux
+                    Tailwind CSS
+                    Express
+                    PostgreSQL
+                    Node.js
+                </p>
+                <button className={styles.GitHub}></button>
+                <button className={styles.Deploy}></button>
+
+            </div>
+
+
 
 
         </div>
